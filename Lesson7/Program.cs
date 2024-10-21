17b58f70f16f5cdf58ff68eb39bae074fc5c0ac8
ekaterina-er <katia.ermakova.83@gmail.com>
2024-10-21 21:29:41 +0300

    Add files via upload

---
+++ Lesson7/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Lesson7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя файла:");
+            string path = Console.ReadLine() + ".txt";
+
+            if (!File.Exists(path))
+            {
+                using (File.Create(path));
+            }
+
+            while (true)
+            {
+                Console.WriteLine($"Меню:\n1 - Ввод текста для записи\n2 - Дополнение текста\n3 - Чтение записанного текста\nВыберите действие:");
+                string? textinput = Console.ReadLine();
+
+                if (int.TryParse(textinput, out int menu))
+                {
+                    switch (menu)
+                    {
+                        case 1:
+                            Writetxt();
+                            break;
+                        case 2:
+                            ReWritetxt();
+                            break;
+                        case 3:
+                            Readtxt();
+                            break;
+                        default:
+                            Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некорректное значение. Попробуйте снова.");
+                }
+            }
+
+            void Writetxt()
+            {
+                Console.WriteLine("Введите текст для записи:");
+                string? Text = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(Text))
+                {
+                    File.WriteAllText(path, Text);
+                }
+                else
+                {
+                    Console.WriteLine("Пустой ввод. Текст не был записан.");
+                }
+            }
+
+            void ReWritetxt()
+            {
+                Console.WriteLine("Введите текст для записи:");
+                File.AppendAllText(path, Console.ReadLine());
+            }
+
+            void Readtxt()
+            {
+                FileInfo fileInfo = new FileInfo(path);
+
+                if (fileInfo.Length == 0)
+                {
+                    Console.WriteLine("Файл пустой.");
+                }
+                else
+                {
+                    string Text = File.ReadAllText(path);
+                    Console.WriteLine("Содержимое файла:");
+                    Console.WriteLine(Text);
+                }
+            }
+        }
+    }
+}
+
